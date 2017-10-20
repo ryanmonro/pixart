91cd41621e7d443fcbfe86b98a4dab40f011ba3a
ryanmonro <ryan@ryanmonro.info>
2017-10-20 14:37:06 +1100

    3: 1000 divs

--- pixart.js
+++ pixart.js
@@ -1,7 +1,12 @@
-
-
 $('#set-color').on('click', function(event){
   event.preventDefault();
   var newColor = $('#color-field').val();
   $('.brush').css({"background": newColor});
 })
+
+// draw 1000 .square divs
+for (var d = 0; d < 1000; d++){
+  var newDiv = $('<div>');
+  newDiv.addClass('square');
+  $('body').append(newDiv);
+}
