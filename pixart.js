ce6de2a7c3ebb149d0231e348b35b8889f4bf636
ryanmonro <ryan@ryanmonro.info>
2017-10-20 15:13:20 +1100

    7: stamp

--- pixart.js
+++ pixart.js
@@ -5,11 +5,35 @@ $('#set-color').on('click', function(event){
   $('.brush').css({"background": newColor});
 })
 
+// set stamp with movie poster from OMDB 
+$('#set-brush').on('click', function(event){
+  event.preventDefault();
+  var searchString = $('#movie-field').val();
+  var options = {
+    url: 'http://omdbapi.com/',
+    method: 'get',
+    data: {
+      s: searchString,
+      apikey: '2f6435d9'
+    }
+  }
+  var appendMovies = function(response){
+    var poster = response.Search[0].Poster;
+    $('.brush').css({"background": "url('" + poster + "')"});
+  }
+  $.ajax(options).done(appendMovies) 
+})
+
 // draw 1000 .square divs
-for (var d = 0; d < 1000; d++){
-  var newDiv = $('<div>');
-  newDiv.addClass('square');
-  $('body').append(newDiv);
+for (var y = 0; y < 50; y++){
+  var outerDiv = $('<div>');
+  outerDiv.addClass('square-row');
+  for (var x = 0; x < 20; x++){
+    var newDiv = $('<div>');
+    newDiv.addClass('square');
+    outerDiv.append(newDiv);
+  }
+  $('body').append(outerDiv);
 }
 
 // set color of square on click
@@ -17,6 +41,6 @@ $('.square').on('mouseover', function(event){
   $square = $(event.target);
   var color = $('.brush').css("background");
   console.log(color);
-  $square.css({"background": color});
-  
+  $square.css({"background": color});  
 })
+
