77ac507bf1e8c123d158a4f9854bb91612fa625c
ryanmonro <ryan@ryanmonro.info>
2017-10-20 14:41:11 +1100

    4: set square to green on click

--- pixart.js
+++ pixart.js
@@ -1,3 +1,4 @@
+// set color of brush with form
 $('#set-color').on('click', function(event){
   event.preventDefault();
   var newColor = $('#color-field').val();
@@ -10,3 +11,11 @@ for (var d = 0; d < 1000; d++){
   newDiv.addClass('square');
   $('body').append(newDiv);
 }
+
+// set color of square on click
+$('.square').on('click', function(event){
+  $square = $(event.target);
+  console.log($square);
+  $square.css({"background": "green"});
+  
+})
